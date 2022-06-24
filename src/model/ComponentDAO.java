0f42e49316a7e2603b234425c09dee6795bbb1a9
DESKTOP-HJL050Q\ez220409 <moonx297@crk.umn.edu>
2022-06-24 17:58:12 +0900

    062422 코드 수정 3 - 데이터까지 연동 완성

--- src/model/ComponentDAO.java
+++ src/model/ComponentDAO.java
@@ -31,7 +31,7 @@ public class ComponentDAO {
 	
 	public ObservableList<Component> getComponentList(){
 		
-		String SQL = "SELECT * FROM component_stock";
+		String SQL = "SELECT * FROM dog_master";
 		ObservableList<Component> componentList = FXCollections.observableArrayList();
 		
 		try {
@@ -40,7 +40,7 @@ public class ComponentDAO {
 			
 			while (rs.next()) {
 				
-				Component component = new Component(rs.getString("classify"),rs.getString("component_age"), rs.getInt("com_count"), rs.getString("com_place"));
+				Component component = new Component(rs.getString("classify"),rs.getString("age"), rs.getInt("com_count"), rs.getString("com_place"));
 				componentList.add(component);
 			}
 			
@@ -67,7 +67,7 @@ public class ComponentDAO {
 
 		try {
 			
-			String SQL = "DELETE FROM component_stock";
+			String SQL = "DELETE FROM dog_master";
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.executeQuery();
 			pstmt.close();
@@ -85,7 +85,7 @@ public class ComponentDAO {
 		try {
 			
 			System.out.println(componentList.size());
-			String SQL = "INSERT INTO component_stock";
+			String SQL = "INSERT INTO dog_master";
 			SQL += "(age,classify,com_count,com_place)";
 			SQL += " VALUES(?,?,?,?)";
 				
